3d3d07dc2deae688532b73153b77bc6e74fbbe51
pesoplumajgl <135280474+pesoplumajgl@users.noreply.github.com>
2023-06-02 17:20:40 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,131 @@
+basic.showLeds(`
+    # . . . .
+    . # . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(2000)
+basic.showLeds(`
+    # . . . .
+    # . . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(3000)
+basic.showLeds(`
+    # # . . .
+    # # . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(3000)
+basic.showLeds(`
+    # . . . .
+    . . . . .
+    # # . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(5000)
+basic.showLeds(`
+    # . . . .
+    . # . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(2000)
+basic.showLeds(`
+    # # . . .
+    # # . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(3000)
+basic.showLeds(`
+    # . . . .
+    . . . . .
+    # # . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(5000)
+basic.showLeds(`
+    . # . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(4000)
+basic.showLeds(`
+    # . . . .
+    . # . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(2000)
+basic.showLeds(`
+    # . . . .
+    # # . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(3000)
+basic.showLeds(`
+    # . . . .
+    . # . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(2000)
+basic.showLeds(`
+    # # . . .
+    # . . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(3000)
+basic.showLeds(`
+    # . . . .
+    . . . . .
+    # # . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(5000)
+basic.showLeds(`
+    # . . . .
+    . # . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(2000)
+basic.showLeds(`
+    # # . . .
+    . # . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(3000)
+basic.showLeds(`
+    # . . . .
+    . # . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(2000)
+basic.forever(function () {
+	
+})
